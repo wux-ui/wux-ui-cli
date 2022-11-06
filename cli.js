cd274fcba1545abf5fec11c6d88efbb79a1b8c14
Slouchwind <2241051890@qq.com>
2022-11-06 16:30:38 +0800

    v0.0.16

--- cli.js
+++ cli.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 import envinfo from 'envinfo';
-import chalk from 'chalk';
 import fs from 'fs';
-import { handleArgv } from './lib/argv.js';
 import inquirer from 'inquirer';
 import axios from 'axios';
+import spawn from 'cross-spawn';
+
+import { handleArgv } from './lib/argv.js';
+import { err, color, warnInfo, handleStatus } from './lib/log.js';
+import { needUpdate } from './lib/update.js';
+import path from 'path';
 const argv = handleArgv();
 const { processedArgv } = argv;
-const color = {
-    react: chalk.hex('#59c9f1'),
-    wux: chalk.hex('#5064e1'),
-}
 
 //wux-ui -d [cmd]
 if (processedArgv.includes('-d') || processedArgv.includes('--debug')) console.log(argv);
@@ -22,24 +22,30 @@ if (processedArgv.includes('ls')) {
         .then(env => {
             const { npmPackages } = JSON.parse(env);
             if (npmPackages.react === 'Not Found') {
-                console.log(chalk.red(
-                    'The node package ' +
-                    color.react('react') +
-                    ' is not found'
-                ));
+                err(`The node package ${color.react('react')} is not found`);
             }
             else {
                 console.log(`${color.react('react')}: v${npmPackages.react.installed}`);
-                fs.readdir('./src/wux-ui', (err, files) => {
-                    if (err) console.log(chalk.red(
-                        'You don\'t have ' +
-                        color.wux('wux-ui') +
-                        ' yet'
-                    ));
+                fs.readdir('./src/wux-ui-react', (err, files) => {
+                    if (err) {
+                        err(`You don't have ${color.wux('wux-ui-react')} yet`);
+                        inquirer
+                            .prompt([{
+                                type: 'confirm',
+                                name: 'install',
+                                message: 'Do you want to install wux-ui-react?',
+                                default: false
+                            }])
+                            .then(({ install }) => {
+                                if (install) {
+                                    spawn('git', ['clone', 'https://github.com/wux-ui/wux-ui-react', '.\\src\\wux-ui-react'], { stdio: 'inherit' });
+                                }
+                            })
+                    }
                     else if (files.includes('package.json')) {
-                        fs.readFile('./src/wux-ui/package.json', (e, data) => {
+                        fs.readFile('./src/wux-ui-react/package.json', (e, data) => {
                             if (e) throw e;
-                            else console.log(`${color.wux('wux-ui')}: v${JSON.parse(data).version}`);
+                            else console.log(`${color.wux('wux-ui-react')}: v${JSON.parse(data).version}`);
                         });
                     }
                 })
@@ -50,12 +56,12 @@ if (processedArgv.includes('ls')) {
 //wux-ui i <pkg>
 if (processedArgv.includes('i')) {
     const pkg = processedArgv[processedArgv.indexOf('i') + 1];
-    if (pkg === undefined) console.log(chalk.red('You need write the pkg name to install'));
+    if (pkg === undefined) err('You need write the pkg name to install');
     else {
         switch (pkg) {
             case 'wux-ui':
-                console.log(chalk.red('You needn\'t to install the wux.css, you should import that css by the CDN'));
-                console.log(chalk.black.bgYellow('SEE') + ' ' + color.wux('https://docs.wux-ui.tk/start/download.html'));
+                err('You needn\'t to install the wux.css, you should import that css by the CDN');
+                warnInfo('SEE', color.wux('https://docs.wux-ui.tk/start/download.html'));
                 inquirer
                     .prompt([{
                         type: 'confirm',
@@ -66,20 +72,29 @@ if (processedArgv.includes('i')) {
                     .then(({ install }) => {
                         if (install) axios
                             .get('https://debug.wux-ui.tk/dist/wux.css')
-                            .then(r => {
-                                if (r.status === 200) {
-                                    console.log(chalk.green(`GET ${r.status} ${r.statusText}`));
-                                    fs.writeFile('wux.css', r.data, err => {
-                                        if (err) throw err;
-                                        console.log('The file is installed');
-                                    })
-                                }
-                                else console.log(chalk.red(`GET ${r.status} ${r.statusText}`));
-                            })
+                            .then(r => handleStatus(r.status, r.statusText, _ =>
+                                fs.writeFile('wux.css', r.data, err => {
+                                    if (err) throw err;
+                                    console.log('The file is installed');
+                                })
+                            ));
                     });
                 break;
 
-            default: console.log(chalk.red(`There isn\'t a pkg of ${pkg}`));
+            default: err(`There isn't a pkg of ${pkg}`);
         }
     }
+}
+
+//wux-ui update
+if (processedArgv.includes('update')) {
+    const { version } = JSON.parse(fs.readFileSync(path.join(argv.defaultArgv[1], '../package.json')).toString());
+    needUpdate(version).then(({ need, nowVersion }) => {
+        if (need) {
+            spawn('npm', ['i', 'wux-ui-cli@latest', '-g'], { stdio: 'inherit' }).addListener('close', _ => {
+                console.log(`Update ${color.wux('wux-ui-cli')} ${version} -> ${nowVersion}`);
+            });
+        }
+        else console.log('There is no new version');
+    }, (err) => { throw err });
 }
